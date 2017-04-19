25ae152e65f93c9d431da2fe6c098cbc7dd54231
areee <areee@users.noreply.github.com>
2017-04-19 22:42:56 +0300

    exercise started (and ≈50% done)

---
+++ src/PictureGraphics.java
@@ -0,0 +1,31 @@
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.geom.AffineTransform;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author ylhaart
+ */
+public class PictureGraphics extends JPanel {
+
+    Image i;
+
+    public void setImage(Image i) {
+        this.i = i;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g.create();
+        
+        if(i!=null){
+            AffineTransform imageAdjust = AffineTransform.getScaleInstance((float) getWidth() / i.getWidth(this), (float) getHeight() / i.getHeight(this));
+            g2.drawImage(i, imageAdjust, this);
+        }
+    }
+
+}
